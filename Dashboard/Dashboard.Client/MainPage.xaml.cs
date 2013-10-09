0dcef07cfc75978b548b8204a378e60079cfc844
nvsnkv <nvsnkv@live.com>
2013-10-09 23:08:01 +0400

    cleaned up from visualtreeviewer

--- Dashboard/Dashboard.Client/MainPage.xaml.cs
+++ Dashboard/Dashboard.Client/MainPage.xaml.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using VisualTreeViewer;
+﻿using System.Windows.Controls;
 
 namespace Dashboard.Client
 {
@@ -18,17 +7,6 @@ namespace Dashboard.Client
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-        {
-            var builder = new VisualTreeBuilder((FrameworkElement)VisualTreeHelper.GetRoot(LayoutRoot));
-            TreeViewer.ItemsSource = new[] { builder.Tree };
         }
     }
 }
